d58414cc6cc80866704e444f5157a47be6955a53
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-11-25 12:46:36 +0100

    Class Message implementation

--- src/prodcons/buffer/Message.java
+++ src/prodcons/buffer/Message.java
@@ -1,5 +1,15 @@
 package prodcons.buffer;
 
 public class Message {
+	String msg;
+	int id;
 
+	public Message(int id, String msg) {
+		this.id = id;
+		this.msg = msg;
+	}
+
+	public void printMsg(String msg) {
+		System.out.println(msg + this.id);
+	}
 }
